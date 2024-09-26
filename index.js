920f1ff18970d43c9ee8bbc0447b177226bfd48a
masindes <maasinde.sylvester@gmail.com>
2024-09-26 14:23:45 +0300

    update index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+// Function expression called divide
+const divide = function(a, b) {
+    if (b === 0) {
+      throw new Error("Cannot divide by zero");
+    }
+    return a / b;
+  };
+  
+  // Arrow function called square
+  const square = (x) => x * x;
+  
+  // Arrow function called add
+  const add = (a, b) => a + b;
